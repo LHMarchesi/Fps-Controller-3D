bccaadd37baf773adbc8325a3ab3ee2ec840a5d6
eli094 <elizabethiscool094@gmail.com>
2024-11-04 20:53:08 -0300

    EnemigoPersigueSiempreJugador
    Pude hacer que el enemigo persiga siempre al jugador.
    
    No se buguea con las paredes :)

--- Assets/Scripts/EnemySeeker/AiEnemy.cs
+++ Assets/Scripts/EnemySeeker/AiEnemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,9 +12,23 @@ public class AiEnemy : MonoBehaviour
 
     public NavMeshAgent iA;
 
+    //public Animation Anim; (Esto se debería activar cuando el Vigía tenga las animaciones)
+    //public string NombreAnimacionCaminar; (para cuando esté la animación de caminar. Tipo, se pone el nombre) 
+    //public string NombreAnimacionAtacar;
+
     void Update()
     {
         iA.speed = speed;
         iA.SetDestination(objective.position);
+
+        /*if(iA.velocity == Vector3.zero)
+         {
+            Anim.CrossFade(NombreAnimacionAtacar);
+         }
+        else
+        {
+            Anim.CrossFade(NombreAnimacionCaminar);
+        }
+        */
     }
 }
